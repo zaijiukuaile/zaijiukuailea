5eafa83ef3c42bcd8aa4a957aa03378608559b25
zaijiukuaile <130519902+zaijiukuaile@users.noreply.github.com>
2023-07-15 22:58:38 +0800

    Update zaijiukuai.py

--- zaijiukuai.py
+++ zaijiukuai.py
@@ -231,4 +231,17 @@ def focus_timer(duration):
 
 # 示例：25分钟的专注时钟
 focus_timer(25)
+focus_timer(25)
+import time
 
+def focus_timer(duration):
+    start_time = time.time()
+    end_time = start_time + duration * 60
+    while time.time() < end_time:
+        remaining_time = int(end_time - time.time())
+        print(f"Remaining Time: {remaining_time//60} minutes {remaining_time%60} seconds")
+        time.sleep(1)
+    print("Time's up! Focus session complete.")
+
+# 示例：25分钟的专注时钟
+focus_timer(25)
